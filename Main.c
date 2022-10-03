c819bbbab0317300693e3dfcae2c12cdfb719b46
Diskrym <louischambrey.lya@gmail.com>
2022-10-03 10:42:24 +0200

    base

--- Main.c
+++ Main.c
@@ -4,10 +4,3 @@
 int main(){
     printf("heeeeeeeeeeeeuhsdvodsfohlieeeeeee");
     return 0;
-<<<<<<< HEAD
-}
-}
-//tentative modiff
-
-// louis la salope
->>>>>>> e8a209831e677a15267ec7b0f4e7265083911445
