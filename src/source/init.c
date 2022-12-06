ba78da2757182e7a180ff6561e384be92ca7bd3d
Diskrym <82980711+Diskrym@users.noreply.github.com>
2022-12-06 10:56:53 +0100

    stats

--- src/source/init.c
+++ src/source/init.c
@@ -177,7 +177,6 @@ void Select_Level (Joueur *joueur, Lvl *lvl, Entité *entité, EffetSon *son)
     }
     if (lvl->Num == 10)
     {
-        printf("aaaaaaaaaaa");
         Load_Level_4_2(joueur,&entité->Petit_rocher, &entité->Gros_rocher,&entité->tronc,&entité->tanguy,lvl);
     }
     if (lvl->Num == 11)
@@ -601,7 +600,7 @@ void Load_Game (Joueur *joueur, Lvl *lvl, Stats *stats)
     FILE* fichier1 = fopen ( nomFichier1 , "r+" );
     if ( fichier1 )
     {
-        fscanf(fichier1,"Total_pièce=%d Total_Tués=%d Total_Mort=%d Dague_Lancées=%d KDA=%f TEMPS_GENE=%f S1=%f S2=%f S3=%f S4=%f S5=%f C1=%s C2=%s C3=%s C4=%s C5=%s",&stats->Total_pièce,&stats->Total_Tués,&stats->Total_Mort,&stats->Dague_Lancées,&stats->KDA,&stats->TEMPS_GENE,&stats->Score_Board[0],&stats->Score_Board[1],&stats->Score_Board[2],&stats->Score_Board[3],&stats->Score_Board[4],&stats->Convert_Score[0],&stats->Convert_Score[1],&stats->Convert_Score[2],&stats->Convert_Score[3],&stats->Convert_Score[4]);
+        fscanf(fichier1,"Total_pièce=%d Total_Tués=%d Total_Mort=%d Dague_Lancées=%d KDA=%f TEMPS_GENE=%f S1=%f S2=%f S3=%f S4=%f S5=%f %s %s %s %s %s",&stats->Total_pièce,&stats->Total_Tués,&stats->Total_Mort,&stats->Dague_Lancées,&stats->KDA,&stats->TEMPS_GENE,&stats->Score_Board[0],&stats->Score_Board[1],&stats->Score_Board[2],&stats->Score_Board[3],&stats->Score_Board[4],&stats->Convert_Score[0],&stats->Convert_Score[1],&stats->Convert_Score[2],&stats->Convert_Score[3],&stats->Convert_Score[4]);
         fclose (fichier1);
     }
 }
